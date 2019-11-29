6a25c9f1929cf8ac7bbf6f20ca188344cad8f4d8
Marlon dos Santos Costa <marlonbcg@gmail.com>
2019-11-28 21:02:36 -0300

    initial commit

---
+++ Autocenter_v2/Autocenter_v2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Autocenter_v2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
