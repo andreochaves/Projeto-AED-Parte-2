c34c77b3ce87c8a1d30ede33b3607ba04984e202
Marlon dos Santos Costa <marlonbcg@gmail.com>
2019-11-30 15:45:48 -0300

    Diagrama de classes iniciado e criado classe Pessoa

---
+++ Autocenter_v2/Autocenter_v2/Models/Pessoa.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Autocenter_v2.Models
+{
+    class Pessoa
+    {
+        protected string nome;
+        protected string cpf;
+        protected string endereco;
+        protected int idade;
+
+        private bool valido = false;
+        public bool verificarIdade()
+        {
+            if (this.idade >= 18)
+            {
+                valido = true;
+            }
+            return valido;
+        }
+
+        public void verificarCpf()
+        {
+
+        }
+    }
+}
