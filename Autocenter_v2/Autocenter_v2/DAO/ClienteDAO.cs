777aab5a69a4bd13ad5e801b821674ca95809499
Marlon dos Santos <marlonbcg@gmail.com>
2019-12-05 00:25:49 -0300

    Adicionado classe ClienteDAO, junto com método para cadastro de clientes no bd

---
+++ Autocenter_v2/Autocenter_v2/DAO/ClienteDAO.cs
@@ -0,0 +1,53 @@
+﻿using Autocenter_v2.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace Autocenter_v2.DAO
+{
+    class ClienteDAO
+    {
+
+        SqlDataReader dr;
+        SqlCommand cmd = new SqlCommand();
+        Connection ConnBD = new Connection();
+
+        public bool cadastrarCliente(Cliente cliente) {
+        cmd.CommandText = @"INSERT INTO Cliente
+           (nome
+           , cpf
+           , endereco
+           , idade
+           , qtd_pontos_fidelidade)
+     VALUES
+            (@nome
+           , @cpf
+           , @endereco
+            , @idade
+           , @qtd_pontos_fidelidade)";  //Comando Sql
+
+            cmd.Parameters.AddWithValue("@nome", cliente.getNome());     //Recebe Valor p/ Pesquisa no BD
+
+            bool usuarioCadastrado = false;
+            try
+            {
+                cmd.Connection = ConnBD.Conectar();     //Abre Conexão com o BD
+                dr = cmd.ExecuteReader();                  //Armazena Informações do BD
+
+                if (dr.HasRows)                            //Verifica se Encontrou Dados no BD
+                {
+                    usuarioCadastrado = true;   
+                }
+                ConnBD.Desconectar();
+            }
+            catch (Exception ex)
+            {
+                ConnBD.Desconectar();
+                Console.WriteLine(ex.Message);
+            }
+
+            return usuarioCadastrado;
+        }
+    }
+}
