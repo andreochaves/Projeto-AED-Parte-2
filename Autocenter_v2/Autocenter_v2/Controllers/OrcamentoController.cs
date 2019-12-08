77f28b8aabaa3d72618ed3ca7edaa7275e7ba3b9
andreochaves <50426776+andreochaves@users.noreply.github.com>
2019-12-07 23:36:53 -0300

    Adicao das Classes OrcaemntoController e DAO

---
+++ Autocenter_v2/Autocenter_v2/Controllers/OrcamentoController.cs
@@ -0,0 +1,45 @@
+﻿using Autocenter_v2.DAO;
+using Autocenter_v2.Models;
+using Autocenter_v2.Utils;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Autocenter_v2.Controllers
+{
+    class OrcamentoController
+    {
+        public static bool cadastrarOrcamento()
+        {
+            bool OrcamentoCadastrado = false;
+
+            Console.WriteLine("CPF do cliente: ");
+            string cpf = Console.ReadLine();
+
+            Console.WriteLine("\nSelecione o Servico: ");
+            string servico = Console.ReadLine();
+
+            Console.WriteLine("Forma de Pagamento: ");
+            string formaPagamento =  Console.ReadLine();
+
+
+            Orcamento orcamento = new Orcamento();
+            orcamento = new Cliente(nome, cpf, endereco, idade);
+            bool cpfValido = Validator.validarCPF(cliente.getCPF());
+
+            if (cpfValido)
+            {
+                ClienteController clienteController = new ClienteController();
+                ClienteDAO clienteDAO = new ClienteDAO();
+
+                clienteCadastrado = clienteDAO.inserirCliente(cliente);
+            }
+            else
+            {
+                Program.msg += "CPF Inválido!\n";
+            }
+
+            return clienteCadastrado;
+        }
+    }
+}
---
+++ Autocenter_v2/Autocenter_v2/DAO/OrcamentoDAO.cs
@@ -0,0 +1,54 @@
+﻿using Autocenter_v2.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace Autocenter_v2.DAO
+{
+    class OrcamentoDAO
+    {
+
+        SqlDataReader dr;
+        SqlCommand cmd = new SqlCommand();
+        Connection ConnBD = new Connection();
+
+        public bool inserirOrcamento(Orcamento orcamento)
+        {
+            cmd.CommandText = @"INSERT INTO Cliente
+           (nome
+           , cpf
+           , endereco
+           , idade
+           , qtd_pontos_fidelidade)
+     VALUES
+            (@nome
+           , @cpf
+           , @endereco
+            , @idade
+           , @qtd_pontos_fidelidade)";  //Comando Sql
+
+            cmd.Parameters.AddWithValue("@nome", cliente.getNome());     //Recebe Valor p/ Pesquisa no BD
+            cmd.Parameters.AddWithValue("@cpf", cliente.getCPF());
+            cmd.Parameters.AddWithValue("@endereco", cliente.getEndereco());
+            cmd.Parameters.AddWithValue("@idade", cliente.getIdade());
+            cmd.Parameters.AddWithValue("@qtd_pontos_fidelidade", cliente.getQtdPontosFidelidade());
+
+            bool usuarioCadastrado = false;
+            try
+            {
+                cmd.Connection = ConnBD.Conectar();     //Abre Conexão com o BD
+                dr = cmd.ExecuteReader();                  //Armazena Informações do BD
+                ConnBD.Desconectar();
+                usuarioCadastrado = true;
+            }
+            catch (Exception ex)
+            {
+                ConnBD.Desconectar();
+                Console.WriteLine(ex.Message);
+                usuarioCadastrado = false;
+            }
+
+            return usuarioCadastrado;
+        }
+}
