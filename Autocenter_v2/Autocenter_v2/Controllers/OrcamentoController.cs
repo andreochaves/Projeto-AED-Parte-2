df39c5344f6621a84d6e1194c0413d8b8b8adcaa
andreochaves <50426776+andreochaves@users.noreply.github.com>
2019-12-08 22:45:36 -0300

    Alteracao da Calasse OrcamentoController

--- Autocenter_v2/Autocenter_v2/Controllers/OrcamentoController.cs
+++ Autocenter_v2/Autocenter_v2/Controllers/OrcamentoController.cs
@@ -9,37 +9,56 @@ namespace Autocenter_v2.Controllers
 {
     class OrcamentoController
     {
+
         public static bool cadastrarOrcamento()
         {
+            string resposta;
+            string servico;
             bool OrcamentoCadastrado = false;
 
-            Console.WriteLine("CPF do cliente: ");
-            string cpf = Console.ReadLine();
+            Console.WriteLine("Nome do cliente: ");
+            string nomeCliente = Console.ReadLine();
 
-            Console.WriteLine("\nSelecione o Servico: ");
-            string servico = Console.ReadLine();
+            Console.WriteLine("\nVeículo:" +
+                "\n1 - Carro" +
+                "\n2 - Moto ");
+            string veiculo = Console.ReadLine();
+            switch (veiculo)
+            {
+                case "1": 
+                    break;
+                case "2":
+                    break;
+                default:
+                    Console.WriteLine("Opçao Invalida,tente novamente!");
+                    break;
+            }
+            Console.WriteLine("Placa: ");
+            string placa = Console.ReadLine();
+
+            Console.WriteLine("Serviço: ");
+            servico = Console.ReadLine();
+            do
+            {
+                Console.WriteLine("Deseja incluir mais algum servico?" +
+                    "\n1 - Sim" +
+                    "\n2 - Não");
+                resposta = Console.ReadLine();
+
+                Console.WriteLine("Serviço: ");
+                servico += "," + Console.ReadLine();
+            } while (!resposta.Equals("2"));
 
             Console.WriteLine("Forma de Pagamento: ");
             string formaPagamento =  Console.ReadLine();
 
+            Console.WriteLine(double valorTotal);
+
 
             Orcamento orcamento = new Orcamento();
-            orcamento = new Cliente(nome, cpf, endereco, idade);
-            bool cpfValido = Validator.validarCPF(cliente.getCPF());
-
-            if (cpfValido)
-            {
-                ClienteController clienteController = new ClienteController();
-                ClienteDAO clienteDAO = new ClienteDAO();
-
-                clienteCadastrado = clienteDAO.inserirCliente(cliente);
-            }
-            else
-            {
-                Program.msg += "CPF Inválido!\n";
-            }
-
-            return clienteCadastrado;
+            orcamento = new Orcamento(nomeCliente, veiculo, placa, servico,formaPagamento,valorTotal);
+           
+            return OrcamentoCadastrado;
         }
     }
 }
